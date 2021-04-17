13725c7ded7ba384da8c6aad028db3264eb3b64a
akmalitb <81341465+akmalitb@users.noreply.github.com>
2021-04-17 19:47:05 +0700

    Menambahkan file data.py
    file ini berisi global variable dari file eksternal dalam bentuk list, sehingga list dari file eksternal dapat digunakan di berbagai file dengan mudah

---
+++ data.py
@@ -0,0 +1,16 @@
+global user, gadget, gadget_borrow_history, gadget_return_history, consumable, consumable_history
+global header_user, header_gadget, header_gadget_borrow_history, header_gadget_return_history, header_consumable, header_consumable_history
+
+user                   = []
+gadget                 = []
+gadget_borrow_history  = []
+gadget_return_history  = []
+consumable             = []
+consumable_history     = []
+
+header_user                   = []
+header_gadget                 = []
+header_gadget_borrow_history  = []
+header_gadget_return_history  = []
+header_consumable             = []
+header_consumable_history     = []
