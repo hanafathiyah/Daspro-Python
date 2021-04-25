4039617c262ae56589e46da410dfab5f8c83c824
hanafathiyah <40686115+hanafathiyah@users.noreply.github.com>
2021-04-25 17:25:43 +0700

    Add files via upload

--- RiwayatPinjam.py
+++ RiwayatPinjam.py
@@ -13,7 +13,7 @@ def riwayatpinjam():
     i = 0
     while (i < len(datas_sort)): # perulangan untuk mencetak data ke layar
         print("ID Peminjaman\t\t:", datas_sort[i][0])
-        print("Nama Peminjam\t\t:", find_user_name_id(datas_sort[i][1])) # translasi id user menjadi nama user
+        print("Nama Pengambil\t\t:", find_user_name_id(datas_sort[i][1])) # translasi id user menjadi nama user
         print("Nama Gadget\t\t:", find_gadget_name_id(datas_sort[i][2])) # translasi id gadget menjadi nama gadget
         print("Tanggal Peminjaman\t:", datas_sort[i][3])
         print("Jumlah\t\t\t:", datas_sort[i][4])
