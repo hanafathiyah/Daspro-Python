37c6978667d820c9d0986777970a00fb9ddf227e
hanafathiyah <40686115+hanafathiyah@users.noreply.github.com>
2021-04-25 12:16:09 +0700

    udah pake komen

--- MenambahItem.py
+++ MenambahItem.py
@@ -1,3 +1,4 @@
+# import file, fungsi, dan prosedur
 import data
 from LoadData import load_data
 from adding_function import is_jumlah_valid
@@ -10,39 +11,40 @@ from adding_function import isGadgetOrConsumable
 from adding_function import banyak_data
 load_data("folder_isi")
 
-def tambah_item():
+# procedure tambah_item
+def tambah_item(): 
     new_item = []
     datas = []
-    id = input("Masukan ID: ")
-    if (not is_id_valid(id)):
+    id = input("Masukan ID: ") # input id
+    if (not is_id_valid(id)): # validasi id : id tidak valid
         print("Gagal menambahkan item karena ID tidak valid.")
-    else: 
-        if (isGadgetOrConsumable(id) == "gadget"):
+    else: # validasi id : id valid
+        if (isGadgetOrConsumable(id) == "gadget"): # mengecek jenis benda berdasarkan id
             datas = data.gadget
         else:
             datas = data.consumable
-        if(is_id_available(id, datas) == True):
+        if(is_id_available(id, datas) == True): # validasi ketersediaan id : id sudah tersedia
             print("Gagal menambahkan item karena ID sudah ada.")
-        else:
-            nama = input("Masukan Nama: ")
-            deskripsi = input("Masukan Deskripsi: ")
-            jumlah = input("Masukan Jumlah: ")
-            if (not is_jumlah_valid(jumlah)):
-                print("Input jumlah tidak valid!")
-            else:
-                rarity = input("Masukan Rarity: ")
-                if (not is_rarity_valid(rarity)):
+        else: # validasi ketersediaan id : id belum tersedia
+            nama = input("Masukan Nama: ") # input nama
+            deskripsi = input("Masukan Deskripsi: ") # input deskripsi
+            jumlah = input("Masukan Jumlah: ") # input jumlah
+            if (not is_jumlah_valid(jumlah)): # validasi jumlah : jumlah tidak valid
+                print("Input jumlah tidak valid!") 
+            else: # validasi jumlah : jumlah valid
+                rarity = input("Masukan Rarity: ") # input rarity
+                if (not is_rarity_valid(rarity)): # validasi rarity : rarity valid
                     print("Input rarity tidak valid!")
-                else:
+                else: # validasi rarity : rarity tidak valid
                     if (isGadgetOrConsumable(id) == "consumable"):
                         new_item = [id,nama,deskripsi,jumlah,rarity]
-                        data.consumable.append(new_item)
+                        data.consumable.append(new_item) # menambahkan item baru pada array consumable
                         print("Item telah berhasil ditambahkan ke database.")
                     else:
-                        tahun_ditemukan = input("Masukan tahun ditemukan: ")
-                        if (not is_tahun_valid(tahun_ditemukan)):
+                        tahun_ditemukan = input("Masukan tahun ditemukan: ") # input tahun ditemukan
+                        if (not is_tahun_valid(tahun_ditemukan)): # validasi tahun ditemukan : tahun tidak valid
                             print("Input tahun tidak valid!")
-                        else:
+                        else: # validasi tahun ditemukan : tahun valid
                             new_item = [id,nama,deskripsi,jumlah,rarity,tahun_ditemukan]
-                            data.gadget.append(new_item)
+                            data.gadget.append(new_item) # menambahkan item baru pada array gadget
                             print("Item telah berhasil ditambahkan ke database.")
