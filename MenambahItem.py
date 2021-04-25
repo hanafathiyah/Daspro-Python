b6a36186753e291e4c3567ff1aae0b8b4f3efa38
hanafathiyah <40686115+hanafathiyah@users.noreply.github.com>
2021-04-25 12:04:37 +0700

    revisi ke sekian kalinya wkwkwkkw
    semua variabel udah divalidasi

--- MenambahItem.py
+++ MenambahItem.py
@@ -1,10 +1,14 @@
 import data
+from LoadData import load_data
+from adding_function import is_jumlah_valid
+from adding_function import is_tahun_valid
 from adding_function import is_id_available
 from adding_function import is_id_valid
 from adding_function import find_raw
 from adding_function import is_rarity_valid
 from adding_function import isGadgetOrConsumable
 from adding_function import banyak_data
+load_data("folder_isi")
 
 def tambah_item():
     new_item = []
@@ -22,18 +26,23 @@ def tambah_item():
         else:
             nama = input("Masukan Nama: ")
             deskripsi = input("Masukan Deskripsi: ")
-            jumlah = int(input("Masukan Jumlah: "))
-            rarity = input("Masukan Rarity: ")
-            if (not is_rarity_valid(rarity)):
-                print("Input rarity tidak valid!")
+            jumlah = input("Masukan Jumlah: ")
+            if (not is_jumlah_valid(jumlah)):
+                print("Input jumlah tidak valid!")
             else:
-                if (isGadgetOrConsumable(id) == "gadget"):
-                    tahun_ditemukan = int(input("Masukan tahun ditemukan: "))
-                    new_item = [id,nama,deskripsi,jumlah,rarity,tahun_ditemukan]
+                rarity = input("Masukan Rarity: ")
+                if (not is_rarity_valid(rarity)):
+                    print("Input rarity tidak valid!")
                 else:
-                    new_item = [id,nama,deskripsi,jumlah,rarity]
-                print("Item telah berhasil ditambahkan ke database.")
-                if (isGadgetOrConsumable(id) == "gadget"):
-                    data.gadget.append(new_item)
-                else:
-                    data.consumable.append(new_item)
+                    if (isGadgetOrConsumable(id) == "consumable"):
+                        new_item = [id,nama,deskripsi,jumlah,rarity]
+                        data.consumable.append(new_item)
+                        print("Item telah berhasil ditambahkan ke database.")
+                    else:
+                        tahun_ditemukan = input("Masukan tahun ditemukan: ")
+                        if (not is_tahun_valid(tahun_ditemukan)):
+                            print("Input tahun tidak valid!")
+                        else:
+                            new_item = [id,nama,deskripsi,jumlah,rarity,tahun_ditemukan]
+                            data.gadget.append(new_item)
+                            print("Item telah berhasil ditambahkan ke database.")
