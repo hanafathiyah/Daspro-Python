6bd471be68982f84ad6f3bb2cb51d6aca9557d78
natsilaban <81223601+natsilaban@users.noreply.github.com>
2021-04-27 18:06:35 +0700

    Update Pencarian_Tahun.py
    udah bisa ngecek role

--- Pencarian_Tahun.py
+++ Pencarian_Tahun.py
@@ -1,36 +1,41 @@
 import data
 from LoadData import load_data
+from adding_function import isAdmin
+from adding_function import isUser
 
 load_data("folder_isi")
 
 def hasil_kategori(kategori,array,tahun):
-  if kategori == '=':
-    return (array[5] == tahun)
-  elif kategori == '>':
-    return (array[5] > tahun)
-  elif kategori == '<':
-    return (array[5] < tahun)
-  elif kategori == '>=':
-    return (array[5] >= tahun)
-  elif kategori == '<=':
-    return (array[5] <= tahun)
+    if kategori == '=':
+        return (array[5] == tahun)
+    elif kategori == '>':
+        return (array[5] > tahun)
+    elif kategori == '<':
+        return (array[5] < tahun)
+    elif kategori == '>=':
+        return (array[5] >= tahun)
+    elif kategori == '<=':
+        return (array[5] <= tahun)
 
 def mencari_tahun() :
-  datas = data.gadget
+    if not isAdmin() or not isUser(): # Jika role bukan admin atau bukan user
+        print("caritahun hanya dapat diakses oleh Admin atau User.")
+    else: # Jika role adalah admin atau user
+        datas = data.gadget
 
-  print(">>> caritahun")
-  tahun = int(input("Masukkan tahun: "))
-  kategori = input("Masukkan kategori: ")
-  print("\nHasil pencarian:\n")
+        print(">>> caritahun")
+        tahun = int(input("Masukkan tahun: "))
+        kategori = input("Masukkan kategori: ")
+        print("\nHasil pencarian:\n")
 
-  is_ketemu = False
-  for array in (datas):
-    if (hasil_kategori(kategori,array,tahun)) :
-      is_ketemu = True
-      print("Nama :", array[1])
-      print("Deskripsi :", array[2])
-      print("Jumlah :", array[3], "buah")
-      print("Rarity :", array[4])
-      print("Tahun Ditemukan :", array[5] ,"\n")
-  if not(is_ketemu):
-    print("Tidak ada gadget yang ditemukan")
+        is_ketemu = False
+        for array in (datas):
+            if (hasil_kategori(kategori,array,tahun)) :
+                is_ketemu = True
+                print("Nama :", array[1])
+                print("Deskripsi :", array[2])
+                print("Jumlah :", array[3], "buah")
+                print("Rarity :", array[4])
+                print("Tahun Ditemukan :", array[5] ,"\n")
+        if not(is_ketemu):
+            print("Tidak ada gadget yang ditemukan")
