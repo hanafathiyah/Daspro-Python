fc873c15ae6c9aed0d498ee486aa74540e858ae9
kofifanhertza <kofifanhertzah@gmail.com>
2021-04-28 15:18:11 +0700

    Validasi tanggal + meminjam hanya boleh 1 gadget

--- MeminjamGadget.py
+++ MeminjamGadget.py
@@ -6,14 +6,23 @@ from adding_function import find_raw
 from adding_function import is_rarity_valid
 from adding_function import isGadgetOrConsumable
 from adding_function import banyak_data
+from adding_function import is_tanggal_valid
+from adding_function import find_gadget_name_id
 
+
+# Keperluan testing
+# from Login import login
 # load_data("folder_isi")
+
 def pinjam():
     role_user = data.user_login[5]
+    id_username = data.user_login[0]
 
     # Validasi role user 
     if (role_user != 'user') :
         return print("Fungsi ini hanya dapat diakses oleh user")
+
+    
 
 
     datas = data.gadget
@@ -23,34 +32,52 @@ def pinjam():
         print("Gagal menambah item karena ID tidak valid.") 
     elif(not is_id_available(id, datas)):
             print("Gagal menambah item karena ID tidak ada.")
+
+    # Verifikasi apakah gadget sudah dipinjam atau belum
+    datax = data.gadget_borrow_history
+    j = 0 
+    while j < banyak_data(datax) : 
+        if id_username == datax[j][1] and id == datax[j][2] : 
+            print(f'Anda tercatat sudah meminjam gadget {find_gadget_name_id(datax[j][2])}')
+            break
+        else :
+            j += 1
+    
     else : 
         tgl = input("Tanggal peminjaman: ")
-        jumlah = int(input("Jumlah peminjaman: "))
-        if jumlah > datas[find_raw(id, datas)][3]:
-            print(f"Item {datas[find_raw(id, datas)][1]} tidak berhasil dipinjam. Jumlah peminjaman terlalu banyak!")
-        elif jumlah <= 0 : 
-            print(f"Item {datas[find_raw(id, datas)][1]} tidak berhasil dipinjam. Jumlah peminjaman harus lebih besar dari 0")
+        if (not is_tanggal_valid(tgl)) : 
+            print("Tanggal peminjaman tidak valid !")
         else :
-            print(f"Item {datas[find_raw(id, datas)][1]} (x{jumlah}) telah berhasil dipinjam!")
-            data.gadget[find_raw(id, datas)][3] -= jumlah
-            is_returned = False
+            jumlah = int(input("Jumlah peminjaman: "))
+            if jumlah > datas[find_raw(id, datas)][3]:
+                print(f"Item {datas[find_raw(id, datas)][1]} tidak berhasil dipinjam. Jumlah peminjaman terlalu banyak!")
+            elif jumlah <= 0 : 
+                print(f"Item {datas[find_raw(id, datas)][1]} tidak berhasil dipinjam. Jumlah peminjaman harus lebih besar dari 0")
+            else :
+                print(f"Item {datas[find_raw(id, datas)][1]} (x{jumlah}) telah berhasil dipinjam!")
+                data.gadget[find_raw(id, datas)][3] -= jumlah
+                is_returned = False
 
-            # Pengambilan id_username
-            id_username = data.user_login[0]
+                # Pengambilan id_username
+                id_username = data.user_login[0]
 
-            # Pembuatan id transaksi otomatis
-            id_transaksi = 1
-            data_peminjaman = data.gadget_borrow_history
-            i = 0
-            available = False
-            while i < banyak_data(data_peminjaman) and available == False:
-                if (datas[i][0] != id_transaksi) and available == True :
-                    available = True
-                else:
-                    i += 1
-                    id_transaksi += 1
+                # Pembuatan id transaksi otomatis
+                id_transaksi = 1
+                data_peminjaman = data.gadget_borrow_history
+                i = 0
+                available = False
+                while i < banyak_data(data_peminjaman) and available == False:
+                    if (datas[i][0] != id_transaksi) and available == True :
+                        available = True
+                    else:
+                        i += 1
+                        id_transaksi += 1
 
-            # Penambahan pada array
-            new_peminjaman = [id_transaksi,id_username, id, tgl, jumlah, is_returned]
-            data.gadget_borrow_history.append(new_peminjaman)
+                # Penambahan pada array
+                new_peminjaman = [id_transaksi,id_username, id, tgl, jumlah, is_returned]
+                data.gadget_borrow_history.append(new_peminjaman)
+
+
+# Keperluan testing
+# login()
 # pinjam()
--- MengembalikanGadget.py
+++ MengembalikanGadget.py
@@ -2,6 +2,7 @@ import data
 from LoadData import load_data
 from adding_function import banyak_data
 from adding_function import find_gadget_name_id
+from adding_function import is_tanggal_valid
 
 
 # Command buat pengen test
@@ -33,22 +34,19 @@ def mengembalikan_gadget() :
                     id_gadget = datas[i][2]
                     Jumlah_Gadget_dipinjam = datas[i][4]
 
-                    # Apabila seorang user meminjam gadget yang sama dalam 2 waktu yang berbeda
-                    if id_gadget in id_gadget_dipinjam : 
-                        jumlah_dipinjam[id_gadget_dipinjam.index(id_gadget)] += datas[i][4]
-                    else :
-                        # Pembuatan Array ID gadget yang dipinjam
-                        id_gadget_dipinjam.append(id_gadget)
+                    
+                    # Pembuatan Array ID gadget yang dipinjam
+                    id_gadget_dipinjam.append(id_gadget)
 
-                        # Pembuatan Array ID transaksi, dibuat untuk memudahkan proses pengembalian
-                        # ID transaksi dipakai karena bersifat unik
-                        gadget_dipinjam.append((datas[i][0]))
+                    # Pembuatan Array ID transaksi, dibuat untuk memudahkan proses pengembalian
+                    # ID transaksi dipakai karena bersifat unik
+                    gadget_dipinjam.append((datas[i][0]))
 
 
-                        Jumlah_Gadget_dipinjam = datas[i][4] 
-                        jumlah_dipinjam.append(Jumlah_Gadget_dipinjam)
+                    Jumlah_Gadget_dipinjam = datas[i][4] 
+                    jumlah_dipinjam.append(Jumlah_Gadget_dipinjam)
 
-                        print(f'{a}. {find_gadget_name_id(id_gadget)}')
+                    print(f'{a}. {find_gadget_name_id(id_gadget)}')
                     i += 1
                     
                 else:
@@ -56,64 +54,70 @@ def mengembalikan_gadget() :
     if len(gadget_dipinjam) == 0 : 
         print('Tidak ada gadget yang dipinjam')
     else : 
-        no_peminjaman = int(input('Masukkan nomor peminjaman : '))
+        no_peminjaman = int(input('Masukkan nomor peminjaman : ')) 
         if no_peminjaman > len(gadget_dipinjam) :
             print('Nomor peminjaman tidak valid')
         else :
             tanggal = input("Tanggal pengembalian : ")
-            i = 0 
-            while i < banyak_data(datas) :
-                        if gadget_dipinjam[no_peminjaman - 1] == datas[i][0] : # Pencarian gadget yang akan dikembalikan menggunakan ID transaksi
-                            id_gadget = id_gadget_dipinjam[no_peminjaman - 1] # Mencari ID gadget yang akan dikembalikan dari array ID gadget yang dipinjam
+            if (not is_tanggal_valid(tanggal)) : 
+                print("Tanggal pengembalian tidak valid !")
+            else : 
+                i = 0 
+                while i < banyak_data(datas) :
+                            if gadget_dipinjam[no_peminjaman - 1] == datas[i][0] : # Pencarian gadget yang akan dikembalikan menggunakan ID transaksi
+                                id_gadget = id_gadget_dipinjam[no_peminjaman - 1] # Mencari ID gadget yang akan dikembalikan dari array ID gadget yang dipinjam
 
-                            print(f'Anda tercatat telah meminjam {jumlah_dipinjam[no_peminjaman - 1]} gadget {find_gadget_name_id(datas[i][2])}')
-                            Jumlah_pengembalian = int(input('Jumlah pengembalian : '))
+                                print(f'Anda tercatat telah meminjam {jumlah_dipinjam[no_peminjaman - 1]} gadget {find_gadget_name_id(datas[i][2])}')
+                                Jumlah_pengembalian = int(input('Jumlah pengembalian : '))
 
-                            # Validasi jumlah yang akan dikembalikan
-                            if Jumlah_pengembalian > jumlah_dipinjam[no_peminjaman - 1] :
-                                print("Jumlah pengembalian harus lebih kecil dari yang dipinjam!")
-                            elif Jumlah_pengembalian <=0 : 
-                                print('Jumlah pengembalian harus lebih besar dari 0!')
+                                # Validasi jumlah yang akan dikembalikan
+                                if Jumlah_pengembalian > jumlah_dipinjam[no_peminjaman - 1] :
+                                    print("Jumlah pengembalian harus lebih kecil dari yang dipinjam!")
+                                elif Jumlah_pengembalian <=0 : 
+                                    print('Jumlah pengembalian harus lebih besar dari 0!')
 
-                            else : 
-                                Total_pengembalian = Jumlah_pengembalian # Untuk di array pengembalian
-                                # Proses pengembalian gadget
-                                j = 0
-                                while j < banyak_data(datas) :
-                                    if (datas[j][1] == id_username) and (datas[j][5] == 'False') and (datas[j][2] == id_gadget) :
-                                        id_transaksi = datas[j][0]
-                                        if Jumlah_pengembalian >= datas [j][4] : 
-                                            Jumlah_pengembalian -= datas[j][4]
-                                            datas[j][4] = 0 
-                                            datas[j][5] = 'True'
+                                else : 
+                                    Total_pengembalian = Jumlah_pengembalian # Untuk di array pengembalian
+
+                                    # Proses pengembalian gadget
+                                    j = 0
+                                    while j < banyak_data(datas) :
+                                        if (datas[j][1] == id_username) and (datas[j][5] == 'False') and (datas[j][2] == id_gadget) :
+                                            id_transaksi = datas[j][0]
+                                            if Jumlah_pengembalian >= datas [j][4] : 
+                                                Jumlah_pengembalian -= datas[j][4]
+                                                datas[j][4] = 0 
+                                                datas[j][5] = 'True'
+                                            else :
+                                                datas[j][4] -= Jumlah_pengembalian
+                                                Jumlah_pengembalian = 0
+                                            j += 1
                                         else :
-                                            datas[j][4] -= Jumlah_pengembalian
-                                            Jumlah_pengembalian = 0
-                                        j += 1
-                                    else :
-                                        j += 1
-                                    
+                                            j += 1
+                                        
 
-                                # Pembuatan id pengembalian otomatis
-                                id_pengembalian = 1
-                                data_pengembalian = data.gadget_return_history
-                                i = 0
-                                available = False
-                                while i < banyak_data(data_pengembalian) and available == False:
-                                    if (datas[i][0] != id_pengembalian) and available == True :
-                                        available = True
-                                    else:
-                                        i += 1
-                                        id_pengembalian += 1
+                                    # Pembuatan id pengembalian otomatis
+                                    id_pengembalian = 1
+                                    data_pengembalian = data.gadget_return_history
+                                    i = 0
+                                    available = False
+                                    while i < banyak_data(data_pengembalian) and available == False:
+                                        if (datas[i][0] != id_pengembalian) and available == True :
+                                            available = True
+                                        else:
+                                            i += 1
+                                            id_pengembalian += 1
 
-                                # Penambahan pada array
-                                new_pengembalian = [id_pengembalian,id_transaksi,tanggal, Total_pengembalian]
-                                data.gadget_return_history.append(new_pengembalian)
-                                print(f'Item {find_gadget_name_id(id_gadget)} (x{Total_pengembalian}) berhasil dikembalikan!')
+                                    # Penambahan pada array
+                                    new_pengembalian = [id_pengembalian,id_transaksi,tanggal, Total_pengembalian]
+                                    data.gadget_return_history.append(new_pengembalian)
+                                    print(f'Item {find_gadget_name_id(id_gadget)} (x{Total_pengembalian}) berhasil dikembalikan!')
 
-                            break
-                        else : 
-                            i += 1
+                                break
+                            else : 
+                                i += 1
     
 # login()
 # mengembalikan_gadget()
+# print(data.gadget_borrow_history, '\n')
+# print(data.gadget_return_history)
