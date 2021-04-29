574d96cad801f5d5f965d147277b40af8bf0e52a
natsilaban <81223601+natsilaban@users.noreply.github.com>
2021-04-29 10:00:26 +0700

    Update Login.py
    revisi karena bisa login pake username/password orang lain

--- Login.py
+++ Login.py
@@ -4,34 +4,23 @@ from adding_function import banyak_data
 
 # load_data("folder_isi")
 
-def is_username_valid(user_username,datas):
+def is_login_valid(user_username,user_password,datas):
     i = 0
     available = False
     while i < banyak_data(datas) and available == False:
-        if (datas[i][1] == user_username):
-            available = True
-        else:
-            i += 1
-    return available
-
-def is_password_valid(user_password,datas):
-    i = 0
-    available = False
-    while i < banyak_data(datas) and available == False:
-        if (datas[i][4] == user_password):
-            available = True
+        if (datas[i][1] == user_username) and (datas[i][4] == user_password):
+              available = True
         else:
             i += 1
     return available
     
 def login():
     datas = data.user
-    print(">> login")
     user_username = input("Masukkan username: ")
     user_password = input("Masukkan password: ")
 
      # Mengulang sampai username dan password benar
-    while (is_username_valid(user_username,datas) == False) or (is_password_valid(user_password,datas) == False):
+    while (is_login_valid(user_username,user_password,datas) == False):
         print("\nMasukan username/password salah")
         user_username = input("\nMasukkan username: ")
         user_password = input("Masukkan password: ")
